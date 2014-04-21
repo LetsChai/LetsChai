c3370fff3d36a6a461ed0392f607a771f7cd728e
kedar <kedariyer26@gmail.com>
2014-04-20 20:26:26 -0700

    Semi-thank you

--- app/jongo/types/FacebookAccessToken.java
+++ app/jongo/types/FacebookAccessToken.java
@@ -7,13 +7,39 @@ import java.util.Date;
 /**
  * Created by kedar on 4/7/14.
  */
-public class FacebookAccessToken extends FacebookClient.AccessToken {
+public class FacebookAccessToken {
 
-    private int fb_uid;
+    protected String userId;
+    protected String accessToken;
+    protected Date expires;
 
-    public FacebookAccessToken () {}
+    public FacebookAccessToken (FacebookClient.AccessToken token) {
+        this.accessToken = token.getAccessToken();
+        this.expires = token.getExpires();
+    }
+    public void setUserId (String id) {
+        userId = id;
+    }
 
-    public void setId (int id) {
-        fb_uid = id;
+    public String getUserId () {
+        return userId;
+    }
+
+    public static FacebookAccessToken fromQueryString (String query) {
+        return new FacebookAccessToken(FacebookClient.AccessToken.fromQueryString(query));
+    }
+
+    public static FacebookAccessToken fromQueryString (String query, String userId) {
+        FacebookAccessToken instance = fromQueryString(query);
+        instance.setUserId(userId);
+        return instance;
+    }
+
+    public String getAccessToken () {
+        return accessToken;
+    }
+
+    public Date getExpires () {
+        return expires;
     }
 }
