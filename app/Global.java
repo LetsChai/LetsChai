9c3eceec3011507fd000f90fe7c1baf54675030c
kedar <kedariyer26@gmail.com>
2014-07-30 16:37:11 +0530

    Global created

---
+++ app/Global.java
@@ -0,0 +1,18 @@
+import play.Application;
+import play.GlobalSettings;
+
+/**
+ * Created by kedar on 7/30/14.
+ */
+public class Global extends GlobalSettings {
+
+    @Override
+    public void onStart(Application app) {
+
+    }
+
+    @Override
+    public void onStop (Application app) {
+
+    }
+}
