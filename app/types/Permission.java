cfdc0ace53306de169232700d56f4701f218f9fc
kedar <kedariyer26@gmail.com>
2014-08-01 13:18:13 +0530

    merge with master Permissions

--- app/types/Permission.java
+++ app/types/Permission.java
@@ -5,12 +5,8 @@ package types;
  */
 public enum Permission {
     INSTALLED, PUBLIC_PROFILE, USER_BIRTHDAY, USER_FRIENDS, EMAIL, USER_LOCATION, USER_PHOTOS, USER_EDUCATION_HISTORY,
-<<<<<<< HEAD
-    USER_RELATIONSHIPS;
-=======
     USER_RELATIONSHIPS, USER_ABOUT_ME, USER_ACTIVITIES, USER_HOMETOWN, USER_RELATIONSHIP_DETAILS,
     USER_WORK_HISTORY;
->>>>>>> master
 
     public String lowerCase () {
         return this.toString().replace('_', ' ').toLowerCase();
